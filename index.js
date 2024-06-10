9aed988e10aa8aab47c51ba1e0bea5a6761aae0b
Patricia <166408575+Trish-Cal@users.noreply.github.com>
2024-06-10 08:01:29 -0500

    assignment started

---
+++ index.js
@@ -0,0 +1,15 @@
+// Prompt the user for froyo flavors upon entering site. 
+
+// Plan - create an alert allowing user to input a string of names separated by commas
+
+// User's input string is split into an array of strings. 
+
+// Use a loop to iterate through the array of flavors.
+
+// Use an object to keep count of how many orders there are of each flavor.
+
+// Correctly count the number of flavors in users input. 
+
+// Organize the frequencies of elements in an array into a function that returns an object. 
+
+// Use meanful and consistent names. 
